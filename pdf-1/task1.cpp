bb8b9f7aa668b4d976389d894cca5957408e3c10
Krishnachandarana11 <158564518+Krishnachandarana11@users.noreply.github.com>
2024-05-10 15:02:33 +0530

    Add files via upload

---
+++ pdf-1/task1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    cout << "Cube of " << num << " is: " << num * num * num << endl;
+    return 0;
+}
+
